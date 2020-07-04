558f7666f8b83234408968f7836789b20eb5f89a
andrey.popkov <andrey.popkov@latoken.com>
2020-07-05 02:07:49 +0300

    fix

---
+++ README.md
@@ -0,0 +1,5 @@
+
+
+<code>python3 run.py</code>
+
+The application will be available on 127.0.0.1:5000 in any browser.
--- app/frontend.py
+++ app/frontend.py
@@ -1,6 +1,7 @@
-from flask import Blueprint, render_template, flash, redirect, url_for, request
+from flask import Blueprint, render_template, flash, redirect, url_for, request, app
 from flask_bootstrap import __version__ as FLASK_BOOTSTRAP_VERSION
 from flask_nav.elements import Navbar, View, Subgroup, Link, Text, Separator
+
 
 from app.forms import TextInputForm
 from app.nav import nav
@@ -10,16 +11,22 @@ from app.src.sentiment_analyzer import SentimentAnalyzer
 frontend = Blueprint('frontend', __name__)
 
 nav.register_element('frontend_top', Navbar(
-    View('Respalizer', '.index'),
-    View('Home', '.index'),
-    View('Sentiment form', '.sentiment_form'),
+    View('Detector', '.index'),
     View('Debug-Info', 'debug.debug_root'),
     Text('Using Flask-Bootstrap {}'.format(FLASK_BOOTSTRAP_VERSION)), ))
 
 
-@frontend.route('/')
+MAX_FILE_SIZE = 1024 * 1024 + 1
+@frontend.route("/", methods=["POST", "GET"])
 def index():
-    return render_template('index.html')
+    args = {"method": "GET"}
+    if request.method == "POST":
+        file = request.files["file"]
+        if bool(file.filename):
+            file_bytes = file.read(MAX_FILE_SIZE)
+            args["file_size_error"] = len(file_bytes) == MAX_FILE_SIZE
+        args["method"] = "POST"
+    return render_template("index.html", args=args)
 
 
 @frontend.route('/sentiment', methods=['GET'])
@@ -36,3 +43,5 @@ def predict():
     return render_template("prediction.html", form=TextInputForm(),
                            sentiment_text=sentiment_text,
                            prediction_message=prediction_message)
+
+
